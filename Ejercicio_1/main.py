5b384f7cd28440e91cc8bec084b2f9d18b8005c2
RubLAA <deaguileta@gmail.com>
2025-04-23 00:03:08 +0200

    Update main.py

--- Ejercicio_1/main.py
+++ Ejercicio_1/main.py
@@ -1,13 +1,14 @@
 from HanoiVisualizer import HanoiVisualizer
 from HanoiCalculator import HanoiCalculator
 
-def main(num_blocks = 5):  # Cambia este valor para probar
-    if num_blocks <= 24:
-        visualizer = HanoiVisualizer(num_blocks)
-        visualizer.run()
-    elif num_blocks > 24:
-        visualizer = HanoiCalculator(num_blocks)
-        visualizer.show_result()
+def main():
+     num_blocks = 5  # Cambia este valor para probar
+     if num_blocks <= 24:
+         visualizer = HanoiVisualizer(num_blocks)
+         visualizer.run()
+     elif num_blocks > 24:
+         visualizer = HanoiCalculator(num_blocks)
+         visualizer.show_result()
 
 if __name__ == "__main__":
-    main(int(input("Número de bloques: ")))
+    main()
